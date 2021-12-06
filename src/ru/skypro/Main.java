b0c5c3568c2b99c0f0af092d28dca85637b16cb0
Kaplin Sergey <kaplin@internet.ru>
2021-12-06 20:08:22 +0300

    выполнил дз!

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -29,6 +29,7 @@ public class Main {
         fullName = "Иванов Семён Семёнович";
 
         if (fullName.contains("ё")) {
+
             fullName = fullName.replace("ё", "е");
         }
         System.out.println("Данные ФИО сотрудника — " + fullName);
